cc0fc4bb9cbfa304257667bed44d67462abbbcc0
CodingYetNahi <drszz5dkyd@privaterelay.appleid.com>
2026-08-16 19:48:08 +0530

    Invert entity scaling condition for right movement

--- src/lib/renderEntities.ts
+++ src/lib/renderEntities.ts
@@ -43,7 +43,7 @@ export function drawDetailedAeroplane(
 
   ctx.save();
 
- if (entity.vx < 0) {
+if (entity.vx > 0) {
   ctx.scale(-1, 1);
 }
 
@@ -93,7 +93,7 @@ export function drawDetailedBird(
 
 // Sprites naturally face right.
 // Flip them when travelling left.
-  if (entity.vx < 0) {
+if (entity.vx > 0) {
   ctx.scale(-1, 1);
 }
 
